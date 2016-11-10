ae013a0e4677399490cc73496fcb337808b3ab81
pwaykole <prat.pattu@gmail.com>
2016-11-10 10:54:09 -0500

    testing XHR calls

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,7 @@
 var app = angular.module('testingApp', []);
 
-app.controller('firstController', function ($scope) {
+app.controller('firstController', function ($scope, $http) {
+    $scope.apiKey = '7cadaddb01307afac58eb68ebec1e7ba';
     $scope.title = 'I love angular';
     $scope.destinations = [];
     $scope.newDestination = {
@@ -19,4 +20,20 @@ app.controller('firstController', function ($scope) {
     $scope.removeDestination = function (index) {
         $scope.destinations.splice(index, 1);
     };
+
+    $scope.getWeather = function(destination){
+        $http.get("http://api.openweathermap.org/data/2.5/weather?q="+destination.city+"&appid=" + $scope.apiKey)
+        .then(function success(res){
+            if(res.data.weather)
+            destination.weather = {};
+            destination.weather.main = res.data.weather[0].main;
+            destination.weather.temp = $scope.keltocel(res.data.main.temp);
+        }, function error(err) {
+            console.log(err);
+        })
+    }
+
+    $scope.keltocel = function(temp){
+        return Math.round(temp -273);
+    }
 })
--- app/js/test/unit/testingAngularUnit.js
+++ app/js/test/unit/testingAngularUnit.js
@@ -2,10 +2,15 @@ describe('Testing angular js', function () {
     beforeEach(module('testingApp'));
     describe('Testing angular controller', function () {
         var scope = {};
-        var ctrl;
-        beforeEach(inject(function ($controller) {
+        var ctrl, httpBackend;
+        beforeEach(inject(function ($controller, $httpBackend) {
             ctrl = $controller('firstController', { $scope: scope });
+            httpBackend = $httpBackend;
         }));
+        afterEach(function () {
+            httpBackend.verifyNoOutstandingExpectation();
+            httpBackend.verifyNoOutstandingRequest();
+        });
         it('should initialize the title in the scope', function () {
             expect(scope.title).toBeDefined();
             expect(scope.title).toBe('I love angular');
@@ -60,6 +65,28 @@ describe('Testing angular js', function () {
             expect(scope.destinations[0].city).toBe("Warsaw");
             expect(scope.destinations[0].country).toBe("Poland");
             expect(scope.destinations[1]).toBeUndefined();
+        });
+        it('should update the weather for a specific destination', function () {
+            scope.destination =
+                {
+                    city: "Pune",
+                    country: "India"
+                };
+
+            httpBackend.expectGET("http://api.openweathermap.org/data/2.5/weather?q=" + scope.destination.city + "&appid=" + scope.apiKey)
+            .respond(
+                {
+                    weather: [{ main: 'Clear', description: 'clear sky' }],
+                    main: { temp: 289.713 }
+                }
+            );
+
+            scope.getWeather(scope.destination);
+
+            httpBackend.flush();
+
+            expect(scope.destination.weather.main).toBe("Clear");
+            expect(scope.destination.weather.temp).toBe(17);
         });
     });
 });
